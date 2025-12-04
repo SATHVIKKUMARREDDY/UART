8d83d18b1aedd41a7735897760952e7ea29f66a9
sathvik <kottapusathvikkumarreddy@gmail.com>
2025-12-04 23:16:02 +0100

    changed the read logic

--- src/sc_main.cpp
+++ src/sc_main.cpp
@@ -39,19 +39,38 @@ SC_MODULE(Testbench) {
 
         // Read data from UART RX
         for (uint8_t i = 0; i < 5; ++i) {
+            // wait until rx_count > 0
+            while (true) {
+                tlm_generic_payload stat_trans;
+                uint32_t rx_count = 0;
+                stat_trans.set_command(TLM_READ_COMMAND);
+                stat_trans.set_address(0x08);           // RX count/status
+                stat_trans.set_data_ptr(reinterpret_cast<uint8_t*>(&rx_count));
+                stat_trans.set_data_length(4);
+                stat_trans.set_response_status(TLM_INCOMPLETE_RESPONSE);
+                sc_time delay = SC_ZERO_TIME;
+
+                initiator->b_transport(stat_trans, delay);
+                wait(delay);                            // honor target timing
+                if (stat_trans.is_response_error())
+                    SC_REPORT_FATAL("Testbench", "Status read error");
+
+                if (rx_count > 0) break;
+                wait(sc_time(50, SC_US));               // poll interval
+            }
+
+            // now read one byte from RX (same as before)
             tlm_generic_payload trans;
-            uint8_t data;
+            uint8_t data = 0;
             trans.set_command(TLM_READ_COMMAND);
-            trans.set_address(0x04);
+            trans.set_address(0x04);         // RX register
             trans.set_data_ptr(&data);
             trans.set_data_length(1);
             trans.set_response_status(TLM_INCOMPLETE_RESPONSE);
             sc_time delay = SC_ZERO_TIME;
 
             initiator->b_transport(trans, delay);
-
-            if (trans.is_response_error())
-                SC_REPORT_FATAL("Testbench", "Response error from b_transport");
+            wait(delay);                        // honor read latency
 
             cout << "CPU read RX: " << data << endl;
             wait(sc_time(100, SC_NS));
--- src/uart_tlm.cpp
+++ src/uart_tlm.cpp
@@ -9,11 +9,14 @@ using namespace std;
 // addr 0x00 -> TX register (write)
 // addr 0x04 -> RX register (read)
 // ------------------------ Transmit thread ------------------------
+// ...existing code...
 void UART_TLM::b_transport(tlm_generic_payload& trans, sc_time& delay)
 {
     uint8_t* ptr = trans.get_data_ptr();
     uint64_t addr = trans.get_address();
     tlm_command cmd = trans.get_command();
+
+    const sc_time reg_access_time = sc_time(10, SC_NS);
 
     if (cmd == TLM_WRITE_COMMAND) {
         if (addr == 0x00) {       // TX register
@@ -21,6 +24,9 @@ void UART_TLM::b_transport(tlm_generic_payload& trans, sc_time& delay)
             tx_event.notify();    // trigger transmit thread
             cout << "@" << sc_time_stamp() << " UART TX write: "
                  << (int)(*ptr) << endl;
+            delay += reg_access_time;
+            trans.set_response_status(TLM_OK_RESPONSE);
+            return;
         } else {
             trans.set_response_status(TLM_ADDRESS_ERROR_RESPONSE);
             return;
@@ -33,44 +39,65 @@ void UART_TLM::b_transport(tlm_generic_payload& trans, sc_time& delay)
             } else {
                 *ptr = 0;
             }
+            delay += reg_access_time;
+            trans.set_response_status(TLM_OK_RESPONSE);
+            return;
+        } else if (addr == 0x08) { // RX count/status register (4 bytes)
+            uint32_t cnt = static_cast<uint32_t>(rx_buffer.size());
+            // copy up to requested length
+            size_t copy_len = std::min<size_t>(trans.get_data_length(), sizeof(cnt));
+            std::memcpy(ptr, &cnt, copy_len);
+            delay += reg_access_time;
+            trans.set_response_status(TLM_OK_RESPONSE);
+            return;
         } else {
             trans.set_response_status(TLM_ADDRESS_ERROR_RESPONSE);
             return;
         }
     }
 
-    delay += sc_time(10, SC_NS);
-    trans.set_response_status(TLM_OK_RESPONSE);
+    trans.set_response_status(TLM_COMMAND_ERROR_RESPONSE);
 }
 
 void UART_TLM::transmit_thread()
 {
+    // compute bit and byte time from baud_rate
+    sc_time bit_time = sc_time(1.0 / static_cast<double>(baud_rate), SC_SEC);
+    const unsigned bits_per_frame = 10; // start + 8 data + stop
+    sc_time byte_time = bit_time * bits_per_frame;
+
     while (true) {
         wait(tx_event);
 
         while (!tx_buffer.empty()) {
             uint8_t data = tx_buffer.front();
             tx_buffer.pop();
-            
-            // Simulate baud rate delay (1 bit time at 9600 baud)
-            wait(sc_time(104, SC_US));  // ~10 bits per byte
-            
-            // Loopback: put data in RX buffer
+
+            // simulate full byte transmission time
+            wait(byte_time);
+
+            // Loopback: put data in RX buffer and notify
             rx_buffer.push(data);
             rx_event.notify();
+
+            // log here (one log per byte)
+            cout << "@" << sc_time_stamp() << " UART received: " << (int)data << endl;
         }
     }
 }
-// ------------------------ Receive thread ------------------------
+
 void UART_TLM::receive_thread()
 {
     while (true) {
         wait(rx_event);
 
-        // Just log RX data, don't consume it
+        // If you still want a logging thread, consume after logging
         if (!rx_buffer.empty()) {
-            cout << "@" << sc_time_stamp() << " UART received: "
-                 << (int)rx_buffer.front() << endl;
+            uint8_t v = rx_buffer.front();
+            // optional: log again or perform other processing
+            // cout << "@" << sc_time_stamp() << " RX-log: " << (int)v << endl;
+            // do NOT pop here if the CPU/read should consume the byte
+            // OR pop here if this thread is responsible for consuming RX
         }
     }
 }
